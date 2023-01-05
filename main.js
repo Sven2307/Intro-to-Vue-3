2e7b71f5aa51a8b7b040ecaeb346b8cb571a92e1
Sven2307 <88077251+Sven2307@users.noreply.github.com>
2023-01-05 15:53:14 +0100

    L3 - end

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-<<<<<<< Updated upstream
+
 const product = 'Socks'
-=======
 const app = Vue.createApp({
     data() {
         return {
             product: "Socks",
             image: "./assets/images/socks_blue.jpg",
-            url: "https://test-ke-nl.kenovulo.nl/canon-eos-90d-body"
+            url: "https://acc-ke-nl.kenovulo.nl/canon-eos-90d-body"
         }
     }
 })
->>>>>>> Stashed changes
